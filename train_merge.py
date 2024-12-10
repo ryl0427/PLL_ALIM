a49b7d00dee4ae3080bbcf8955b6f5469d18eb75
unknown <ylren@mail.ustc.edu.cn>
2024-12-10 13:25:16 +0800

    run

--- train_merge.py
+++ train_merge.py
@@ -59,6 +59,16 @@ def train(args, epoch, train_loader,model, loss_fn, loss_cont_fn, optimizer):
         
         # train and save results
         classfy_out, cluster_out, cont_features, cont_labels = model(images_w1, images_s1, plabels, args)
+        
+        print("classfy_out")
+        print(classfy_out)
+        
+        print("cluster_out")
+        print(cluster_out)
+        
+        print("cont_features")
+        print(cont_features)
+        
         print("cont_labels")
         print(cont_labels)
         total_num += plabels.size(0)
