a864b6420baa411df7dad297b5a2f40987004e8d
unknown <ylren@mail.ustc.edu.cn>
2024-12-12 10:44:55 +0800

    ood

--- train_ood.py
+++ train_ood.py
@@ -101,6 +101,7 @@ def train(args, epoch, train_loader,model, loss_fn, loss_cont_fn, optimizer):
         loss_cont = loss_cont_fn(features=cont_features, mask=cont_mask, batch_size=batch_size)
         loss_cls = loss_fn(args, classfy_out, index) # need preds
         print("YES1")
+        print(loss_cls)
         
         if args.loss_weight_mixup !=0:
             lam = np.random.beta(args.alpha, args.alpha)
